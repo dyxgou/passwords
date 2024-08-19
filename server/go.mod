79c2e548796d98ce15c41a71d650fae1dfecc28c
dyxgou <diego5614yt@gmail.com>
2024-08-19 15:57:10 -0500

    Backend: Main files

---
+++ server/go.mod
@@ -0,0 +1,9 @@
+module gihub.com/dyxgou/server
+
+go 1.22.5
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"gihub.com/dyxgou/server/internals/handlers"
+	"gihub.com/dyxgou/server/internals/routes"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	type response struct {
+		Message string `json:"message"`
+	}
+
+	r.Get("/healtz", handlers.ReadinessHandler)
+	r.Get("/", handlers.EntryHandler)
+	r.Get("/error", handlers.ErrorHandler)
+
+	r.Mount("/users", routes.UserRoutes())
+	r.Mount("/passwords", routes.PasswordRoutes())
+
+	log.Println("Server running")
+	http.ListenAndServe(":5000", r)
+}
