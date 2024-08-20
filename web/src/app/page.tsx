e1bd43ee8c14749a48b8ce3bc7ceaf50fb5cbd55
dyxgou <diego5614yt@gmail.com>
2024-08-20 07:53:23 -0500

    Web: Main Page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,29 @@
 "use client"
 
-import PasswordForm from "@/components/PasswordFrom"
+import UsernameForm from "@/components/UsernameForm"
+import { useUser } from "@/states/user"
+import { redirect } from "next/navigation"
+import { useEffect } from "react"
 
 const Page = () => {
-  return (
-    <main className="z-10 gap-10 flex flex-col w-3/5 items-center">
-      <h1 className="md:text-6xl text-4xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to to-neutral-600 font-sans">Descubre que tan segura es tu contraseña</h1>
+  const userId = useUser((state) => state.id)
 
-      <PasswordForm />
-    </main>
+  console.log(userId)
+
+  useEffect(() => {
+    if (userId !== -1) {
+      redirect(`/passwords?id=${userId}`)
+    }
+  }, [userId])
+
+  return (
+    <div className="h-full w-full grid place-items-center">
+      <main className="z-10 gap-10 flex flex-col justify-center items-center w-3/5">
+        <h1 className="md:text-7xl text-4xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to to-neutral-600 font-sans">Descubre que tan segura es tu contraseña</h1>
+
+        <UsernameForm />
+      </main>
+    </div>
   )
 }
 
